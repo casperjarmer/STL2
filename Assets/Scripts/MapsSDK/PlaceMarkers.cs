a422cca9c9d96e493d57baadb2f6ad4b80d2df89
Mads Jensen <madsj20@student.sdu.dk>
2025-05-16 09:40:27 +0200

    Last push

--- Assets/Scripts/MapsSDK/PlaceMarkers.cs
+++ Assets/Scripts/MapsSDK/PlaceMarkers.cs
@@ -30,34 +30,9 @@ namespace Niantic.Lightship.Maps.Samples.GameSample
         // man skal hive alle MarkerSpawner objekter ind i denne array i inspectoren
         public SculptureStats[] sculptures = new SculptureStats[1];
 
-        //public List<Vector2> sculptureLocations = new List<Vector2>();
-
-        //        [SerializeField]
-        //        private FloatingText.FloatingText _floatingTextPrefab;
-        /*
-                [SerializeField]
-                private LayerGameObjectPlacement _sawmillSpawner;
-
-                [SerializeField]
-                private LayerGameObjectPlacement _stoneMasonSpawner;
-
-                [SerializeField]
-                private LayerGameObjectPlacement _strongholdSpawner;
-        */
-        //        private MapGameState.StructureType _placingStructureType;
-        private bool _placingStructure;
-
-        public void StartPlacingStructure(/*MapGameState.StructureType structureType*/)
-        {
-            //_placingStructureType = structureType;
-            //_placingStructure = true;
-        }
-
         private void Start()
         {
-            _placingStructure = true;
             PlaceMarkers();
-            //StartCoroutine(PlaceMarkers());
         }
 
         private void Update()
@@ -79,25 +54,6 @@ namespace Niantic.Lightship.Maps.Samples.GameSample
                 touchPosition = Input.mousePosition;
                 touchDetected = true;
             }
-            /*
-            if (touchDetected)
-            {
-                if (_placingStructure == true)
-                {
-                    PlaceStructure(touchPosition);
-                }
-                else
-                {
-                    CheckForInteractableTouch(touchPosition);
-                }
-            }*/
-        }
-
-        private LatLng ScreenPointToLatLong(Vector3 screenPosition)
-        {
-            var clickRay = _mapCamera.ScreenPointToRay(screenPosition);
-            var pointOnMap = clickRay.origin + clickRay.direction * (-clickRay.origin.y / clickRay.direction.y);
-            return _lightshipMapView.SceneToLatLng(pointOnMap);
         }
 
         private void PlaceMarkers()
@@ -110,97 +66,26 @@ namespace Niantic.Lightship.Maps.Samples.GameSample
                 _markerSpawner.PlaceInstance(sculptureCoordinates);
                 GameObject marker = GameObject.Find("Marker");
                 marker.name = sculptures[i].sculptureName;
-                if(sculptures[i].isCollected)
+                if (sculptures[i].isCollected)
                 {
+                    if (sculptures[i].sculpturePrefab != null)
+                    {
+                        GameObject sculpture = Instantiate(sculptures[i].sculpturePrefab, marker.transform.position, Quaternion.identity);
+                        sculpture.transform.SetParent(marker.transform);
+                        sculpture.transform.localPosition = new Vector3(0, -1, 0);
+                        sculpture.transform.localRotation = Quaternion.identity;
+                        sculpture.transform.localScale = Vector3.one*100;
+                        Destroy(marker.transform.GetChild(1).gameObject);
+                        marker.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.green;
+
+                    }
+                    else { 
                     marker.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.green;
                     marker.transform.GetChild(1).GetComponent<Renderer>().material.color = Color.green;
+                    }
                 }
 
             }
-            /*foreach(GameObject child in buildingLayerGameObject.transform)
-            {
-                LatLng sculptureCoordinates = child.GetComponent<SculptureStatsSc>().coordinates;
-                _markerSpawner.PlaceInstance(sculptureCoordinates); 
-            }*/
-            // Project the touch position onto the map and place a structure prefab there
-            //var structureLatLng = ScreenPointToLatLong(touchPosition);
-            //var statue1LatLng = new LatLng(55.39660748916102, 10.379028264689014); //Mads Hjem
-            var statue1LatLng = new LatLng(55.36680088665288, 10.429245704987402);
-            var statue2LatLng = new LatLng(55.366684144526296, 10.429179869438356);
-            var statue3LatLng = new LatLng(55.366910475573114, 10.429215008732841);
-            var statue4LatLng = new LatLng(55.36676846408718, 10.428824572127436);
-            var statue5LatLng = new LatLng(55.36645981441671, 10.428503174433596);
-
-            //var cameraForward = _mapCamera.transform.forward;
-            //var forward = new Vector3(cameraForward.x, 0f, cameraForward.z).normalized;
-            //var rotation = Quaternion.LookRotation(forward);
-            //_markerSpawner.PlaceInstance(statue1LatLng); //placing marker at statue1 position
-            //_markerSpawner.PlaceInstance(statue2LatLng); //placing marker at statue2 position
-            //_markerSpawner.PlaceInstance(statue3LatLng); //placing marker at statue3 position
-            //_markerSpawner.PlaceInstance(statue4LatLng); //placing marker at statue4 position
-            //_markerSpawner.PlaceInstance(statue5LatLng); //placing marker at statue5 position
-            /*
-            switch (_placingStructureType)
-            {
-                case MapGameState.StructureType.Sawmill:
-                    _sawmillSpawner.PlaceInstance(structureLatLng, rotation);
-                    break;
-
-                case MapGameState.StructureType.StoneMason:
-                    _stoneMasonSpawner.PlaceInstance(structureLatLng, rotation);
-                    // Unlock stone resources on the map when StoneMason built
-                    MapGameState.Instance.EnableResourceProduction(MapGameState.ResourceType.Stone, true);
-                    break;
-
-                case MapGameState.StructureType.Stronghold:
-                    _strongholdSpawner.PlaceInstance(structureLatLng, rotation);
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(_placingStructureType));
-            }
-            
-            // Inform the inventory system so it can save this and alert other systems about the building
-            MapGameState.Instance.StructureBuilt(structureLatLng, _placingStructureType);
-            */
-
-            // uncomment this line to stop placing structures after one is placed
-            //_placingStructure = false;
-        }
-
-        private void CheckForInteractableTouch(Vector3 touchPosition)
-        {
-            var touchRay = _mapCamera.ScreenPointToRay(touchPosition);
-
-            // raycast into scene and see if we hit a map feature
-            if (!Physics.Raycast(touchRay, out var hitInfo))
-            {
-                return;
-            }
-            /*
-            // check if the collider we hit is a feature
-            var hitResourceItem = hitInfo.collider.GetComponent<MapGameResourceFeature>();
-            if (hitResourceItem == null)
-            {
-                return;
-            }
-
-            // check if this resource has any units available to consume
-            if (!hitResourceItem.ResourcesAvailable)
-            {
-                return;
-            }
-
-            // award the player resources for finding this map resource
-            int amount = hitResourceItem.GainResources();
-            MapGameState.Instance.AddResource(hitResourceItem.ResourceType, amount);
-
-            // spawn an animated floating text to show resources being gained
-            var floatingTextPosition = hitInfo.point + Vector3.up * 20.0f;
-            var forward = floatingTextPosition - _mapCamera.transform.position;
-            var rotation = Quaternion.LookRotation(forward, Vector3.up);
-            var floatText = Instantiate(_floatingTextPrefab, floatingTextPosition, rotation);
-            floatText.SetText($"+{amount} {hitResourceItem.ResourceType.ToString()}");*/
         }
     }
 }
--- Assets/Scripts/SculptureStats.cs
+++ Assets/Scripts/SculptureStats.cs
@@ -17,5 +17,6 @@ public class SculptureStats : ScriptableObject
     public double longitude;
     public Sprite image;
     public AudioClip audio;
+    public GameObject sculpturePrefab;
 }
 
